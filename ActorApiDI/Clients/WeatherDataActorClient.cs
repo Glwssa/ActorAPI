e3d693536b3c71d2fd667c7ff035e9323ceaf329
Glwssa <46265146+Glwssa@users.noreply.github.com>
2024-11-07 14:29:09 +0200

    OpenWeather cache

--- ActorApiDI/Clients/WeatherDataActorClient.cs
+++ ActorApiDI/Clients/WeatherDataActorClient.cs
@@ -6,10 +6,11 @@ using System.Collections.Generic;
 using System.Linq;
 using Newtonsoft.Json;
 using ActorApiDI.Domains;
+using Microsoft.Extensions.Caching.Memory;
 
 namespace ActorApiDI.Clients
 {
-    public class WeatherDataActorClient(IHttpClientFactory _httpClientFactory) : IDataActorClient
+    public class WeatherDataActorClient(IHttpClientFactory _httpClientFactory, IMemoryCache _memoryCache) : IDataActorClient
     {
         /// <summary>
         /// Retrives weather data for the requested city.
@@ -23,9 +24,15 @@ namespace ActorApiDI.Clients
             //Valid fields check
             if (request.Param1 is null || request.Header1 is null)
                 throw new BadHttpRequestException("Error: Please provide all the required fields. (Param1/Header1)", 400);
+            //caching check
+            string url = $"/data/2.5/weather?q={request.Param1}&appid={request.Header1}&units=metric";
+            if (_memoryCache.TryGetValue($"OpenWeather {url}", out DataActorResponse result) && result is not null)
+            {
+                return result;
+            }
+
             //Setup HttpRequest
             var client = _httpClientFactory.CreateClient("OpenWeatherClient");
-            string url = $"/data/2.5/weather?q={request.Param1}&appid={request.Header1}&units=metric";
             var response = await client.GetAsync(url);
             //Success response check
             if (!response.IsSuccessStatusCode)
@@ -33,14 +40,17 @@ namespace ActorApiDI.Clients
 
             var stringResult = await response.Content.ReadAsStringAsync();
 
-            //return retrived data in generic format
-            return new DataActorResponse()
+            //return retrived data in generic format and cache it
+            var cachedResult =  new DataActorResponse()
             {
                 ApiName = "OpenWeather",
                 Url = client.BaseAddress + url,
                 Body = stringResult
 
             };
+            _memoryCache.Set($"OpenWeather {url}", cachedResult);
+
+            return cachedResult;
         }
     }
 }
